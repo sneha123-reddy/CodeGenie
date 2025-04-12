6596562590bbef013f9cc0b398d3c6efe5efbbfa
Mahesh-Kira <maheshkiran2001010@gmail.com>
2025-04-12 16:34:45 +0530

    modules

---
+++ codegenie/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    target: "node",
+    entry: "./src/extension.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "extension.js",
+        libraryTarget: "commonjs2",
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    externals: {
+        vscode: "commonjs vscode",
+    },
+};
